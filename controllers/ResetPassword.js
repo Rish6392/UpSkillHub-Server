35e992b8ce1b3614c9da38cb61a5764b89e01093
Rishabh Kumar <157781810+Rish6392@users.noreply.github.com>
2025-06-22 11:55:09 +0000

    resetPassword Controller

---
+++ controllers/ResetPassword.js
@@ -0,0 +1,108 @@
+const User = require("../models/User")
+
+const mailSender = require("../utils/mailSender");
+const bcrypt = require("bcrypt")
+
+
+// resetPasswordToken
+exports.resetPasswordToken = async (requestAnimationFrame, res) => {
+    try {
+        // get email from req ki body
+        const { email } = req.body.email;
+
+        // check user for this email,email verification
+        const user = await user.findOne({ email: email });
+        if (!user) {
+            return res.json({
+                success: false,
+                message: "your Email is not registered with us"
+            });
+        }
+        // generate token
+        const token = crypto.randomUUID();
+        // update user by adding token and expiration time
+        const updatedDetails = await User.findOneAndUpadte(
+            { email: email },
+            {
+                token: token,
+                resetPasswordExpires: Date.now() + 5 * 60 * 1000,
+            },
+            { new: true }
+        );
+        // create url
+        const url = `http://localhost:3000/update-password/${token}`  // frontend ka link
+        // send mail containing the url
+        await mailSender(email,
+            "Password Reset Link",
+            `Password Reset Link:${url}`
+        );
+        // return response
+        return res.json({
+            success: true,
+            message: "Email Sent successfully,please check email and change assword"
+        })
+
+
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong while reset password token"
+        })
+    }
+}
+
+
+// resetPassword
+
+exports.resetPassword = async (req, res) => {
+    try {
+        //data fetch
+        // ye token body main kaise aaya hamne tho token url main pas kiya tha uper=> frontend ne daali hai (ans)
+        const { password, confirmPassword, token } = req.body;
+        // validate
+        if (password != confirmPassword) {
+            return res.json({
+                success: false,
+                message: 'Password not matching'
+            });
+        }
+        // get userdetails from db using token
+        const userDetails = await User.findOne({ token: token });
+        // if no entry- invalid token
+        if (!userDetails) {
+            return res.json({
+                success: false,
+                message: "Token Invalid"
+            })
+        }
+        // token tome check 
+        if (userDetails.resetPasswordExpires < date.now()) {
+            return res.json({
+                success: false,
+                message: "Token is Expired.please regernerate yur token"
+            })
+        }
+        // hash password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        // password update
+        await User.findOneAndUpdate(
+            { token: token },
+            { password: hashedPassword },
+            { new: true },
+        )
+        // return  password 
+        return res.status(200).json({
+            success: true,
+            message: "password reset successfully"
+        })
+    }
+    catch (error) {
+          console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong while reset password "
+        })
+    }
+}
