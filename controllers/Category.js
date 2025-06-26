c929e9dd32d3ad9372d42e2fc4d947e8108a4397
Rishabh Kumar <157781810+Rish6392@users.noreply.github.com>
2025-06-26 16:04:27 +0000

    categoryPageDetails controller

--- controllers/Category.js
+++ controllers/Category.js
@@ -54,3 +54,48 @@ exports.showAllCategory = async(req,res)=>{
         })
     }
 }
+
+
+//categoryPageDetails
+exports.categoryPageDetails = async(req,res)=>{
+    try{
+        //get categoryId
+        const {categoryId} = req.body;
+        //get Courses for specified categoryId
+        const selectedCategory = await Category.findById(categoryId)
+                                                .populate("courses")
+                                                .exec();
+        //vlidation
+        if(!selectedCategory){
+            return res.status(400).json({
+                succes:false,
+                message:'data not found',
+            })
+        }
+        //get courses from diff category
+        const diffrentCategories = await Category.find({
+            _id:{$ne:categoryId},
+        })
+        .populate("courses")
+        .exec();
+        //get top 10/5 selling courses
+
+        //return res
+        return res.status(200).json({
+            success:true,
+            data:{
+                selectedCategory,
+                diffrentCategories,
+                //
+            }
+        });
+
+    }
+    catch(error){
+         console.log(error);
+          return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
