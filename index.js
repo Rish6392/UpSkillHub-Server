4155bb126d9e342071a36b7eb788c05477838367
Rishabh Kumar <157781810+Rish6392@users.noreply.github.com>
2025-07-06 14:48:50 +0000

    index.js file

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 const express = require ("express");
 const app=express();
 
+const userRoutes = require("./routes/User")
+const profileRoutes= require("./routes/Profile")
+const paymentRoutes = require("./routes/Payments")
+const courseRoutes = require("./routes/Course")
+const contactUsRoutes = require("./routes/ContactUs")
 
-const PORT = 3000;
+const db = require("../config/database")
+const cookieParser = require('cookie-parser');
+const cors = require("cors");
+const {cloudinaryConnect} = require("../config/cloudinary")
+require('dotenv').config();
+const fileUpload = require("express-fileupload");
+const port = process.env.PORT || 4000;
 
-app.get('/',(req,res)=>{
-    res.send('Hello World');
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors({
+    origin:"http://localhost:3000",
+    credentials:true,
+}))
+
+app.use(fileUpload({
+    useTempFiles:true,
+    tempFileDir:'/tmp/'
+}));
+
+db.connect();
+
+cloudinaryConnect();
+
+app.use("/api/v1/auth",userRoutes)
+app.use("/api/v1/profile",profileRoutes)
+app.use("/api/v1/course",courseRoutes)
+app.use("/api/v1/payment",paymentRoutes)
+app.use("/api/v1/contact",contactUsRoutes);
+
+
+app.get("/",(req,res)=>{
+    return res.json({
+        success:true,
+        message:"Your server is up and running....."
+    })
 })
 
 app.listen(PORT,()=>{
-    console.log(`Example app is listening on port ${PORT}`);
+    console.log(`Example app is listening on port ${port}`);
 })
